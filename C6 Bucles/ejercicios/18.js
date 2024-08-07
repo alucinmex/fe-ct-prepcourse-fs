4805cfc20225bf607ddd4b9334e5c7d2093b3be0
yonireyesm85 <alucin.mex@gmail.com>
2024-08-06 21:59:04 -0600

    Mi segundo commit

--- C6 Bucles/ejercicios/18.js
+++ C6 Bucles/ejercicios/18.js
@@ -2,6 +2,11 @@ function productoEntreNúmeros(a, b) {
   // Dados dos argumentos "a" y "b", devuelve el producto de todos
   // los números entre a y b (inclusive).
   // Tu código:
-}
+  let acum = a;
 
+  for(let i = a +1; i <= b; i++){
+    acum = acum * i;
+  }
+   return Math.abs(acum)
+  }
 module.exports = productoEntreNúmeros;
--- C6 Bucles/ejercicios/19.js
+++ C6 Bucles/ejercicios/19.js
@@ -2,6 +2,12 @@ function sumarHastaN(n) {
   // La función recibe un número "n" por argumento.
   // Devuelve la suma de todos los números desde 1 hasta n.
   // Tu código:
+
+  let acum = 0
+  for(let i = 1; i <= n; i++){
+    acum = acum + i;
+  }
+  return acum
 }
 
 module.exports = sumarHastaN;
